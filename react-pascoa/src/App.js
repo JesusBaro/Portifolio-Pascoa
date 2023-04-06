060ba350a0f64acd505d8be09befbeadab88d16e
Jésus Baro <118184872+JesusBaro@users.noreply.github.com>
2023-04-06 18:12:13 -0300

    iniciando repositorio

---
+++ react-pascoa/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from './Components/Header.js';
+import Main from './Components/Main.js';
+import Footer from './Components/Footer.js';
+export default function App() {
+  return (
+    <div>
+     <h1>oi</h1>
+     <Header/>
+     <Main/>
+     <Footer/>
+    </div>
+  );
+}
+
+
